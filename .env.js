557824fca35594acbc707c1b3810f76b9f05a047
George Bafaloukas <george.bafaloukas@forgerock.com>
2022-04-04 15:08:33 +0100

    Sanitizing .env file

--- .env.js
+++ .env.js
@@ -1,4 +1,4 @@
-const AM_URL = 'https://forgerock.crbrl.io/am'; // (include the /am)
+const AM_URL = https://auth.forgerock.com/am'; // Required; enter _your_ ForgeRock AM URL
 const DEBUGGER_OFF = true;
 const DEVELOPMENT = true;
 const API_URL = 'https://api.example.com:9443'; // (your resource API server's URL)
@@ -6,10 +6,10 @@ const JOURNEY_LOGIN = 'Login'; // (name of journey/tree for Login)
 const JOURNEY_REGISTER = 'Registration'; // (name of journey/tree for Register)
 const SEC_KEY_FILE = './updatedCerts/api.example.com.key';
 const SEC_CERT_FILE = './updatedCerts/api.example.com.crt';
-const REALM_PATH = 'alpha';
-const REST_OAUTH_CLIENT = 'WebOAuthClientGbafal'; // (name of private OAuth 2.0 client/application)
-const REST_OAUTH_SECRET = 'Password1!'; // (the secret for the private OAuth 2.0 client/application)
-const WEB_OAUTH_CLIENT = 'WebOAuthClient'; // (the name of the public OAuth 2.0 client/application)
+const REALM_PATH = 'alpha'; //Required
+const REST_OAUTH_CLIENT = ''; // (name of private OAuth 2.0 client/application)
+const REST_OAUTH_SECRET = ''; // (the secret for the private OAuth 2.0 client/application)
+const WEB_OAUTH_CLIENT = ''; // (the name of the public OAuth 2.0 client/application)
 const PORT = '9443';
 
 export { 
